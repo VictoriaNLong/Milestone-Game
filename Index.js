b8d43de4e66e98079899b19eaa110f5d03f070e0
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-02-16 20:04:43 -0800

    background images

--- Index.js
+++ Index.js
@@ -1,4 +1,11 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-console.log(c)
+canvas.width = 576
+canvas.height = 1024
+
+const background = new Image();
+background.src = "Assets/MenuScreen.png"
+background.onload = function(){
+c.drawImage(background,0,0);   
+}
