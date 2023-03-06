db87a69accd11767dcbbcf0e21b8092118c9bb45
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-03-05 19:36:30 -0800

    color adjustments

--- Game.js
+++ Game.js
@@ -1,36 +1,34 @@
 const canvas = document.querySelector("#game-canvas");
 const c = canvas.getContext("2d");
 let collectedscore = document.querySelector("#collected");
-let winText = document.querySelector("#winner")
-let starText = document.querySelector("#score")
+let winText = document.querySelector("#winner");
+let starText = document.querySelector("#score");
 
-let helpMenu = document.getElementById("help-menu") 
+let helpMenu = document.getElementById("help-menu");
 
 function hideMenu() {
-  
-if (helpMenu.style.display = "block") {
-  helpMenu.style.display = "none"
-} else {
-  helpMenu.style.display = "block"
-}
+  if ((helpMenu.style.display = "block")) {
+    helpMenu.style.display = "none";
+  } else {
+    helpMenu.style.display = "block";
+  }
 }
 
 function showMenu() {
-  
-  if (helpMenu.style.display = "none") {
-    helpMenu.style.display = "block"
+  if ((helpMenu.style.display = "none")) {
+    helpMenu.style.display = "block";
   } else {
-    helpMenu.style.display = "none"
+    helpMenu.style.display = "none";
   }
-  }
+}
 
 canvas.width = 576;
 canvas.height = 900;
 
 const scaledCanvas = {
   width: canvas.width / 2,
-  height: canvas.height / 2
-}
+  height: canvas.height / 2,
+};
 
 const floorCollisions2D = [];
 for (let i = 0; i < floorCollisions.length; i += 72) {
@@ -66,9 +64,9 @@ starCollect2D.forEach((across, y) => {
         new Sprite({
           position: {
             x: x * 8,
-            y: y * 8
+            y: y * 8,
           },
-          imageSrc: 'Assets/StarCollectSheet.png',
+          imageSrc: "Assets/StarCollectSheet.png",
           frameRate: 4,
           frameSpeed: 15,
         })
@@ -80,56 +78,56 @@ starCollect2D.forEach((across, y) => {
 const gravity = 0.3;
 
 const player = new Player({
- position: {
+  position: {
     x: 100,
-  y: 964,
-},
+    y: 964,
+  },
   collisionBlocks,
   starBlocks,
-  imageSrc:'Assets/FoxIdleSheet.png',
+  imageSrc: "Assets/FoxIdleSheet.png",
   frameRate: 14,
   animations: {
     Idle: {
-      imageSrc:'Assets/FoxIdleSheet.png',
+      imageSrc: "Assets/FoxIdleSheet.png",
       frameRate: 14,
-      frameSpeed: 15
+      frameSpeed: 15,
     },
     IdleLeft: {
-      imageSrc:'Assets/FoxIdleLeftSheet.png',
+      imageSrc: "Assets/FoxIdleLeftSheet.png",
       frameRate: 14,
-      frameSpeed: 10
+      frameSpeed: 10,
     },
     RunRight: {
-      imageSrc:'Assets/FoxRunSheet.png',
+      imageSrc: "Assets/FoxRunSheet.png",
       frameRate: 8,
-      frameSpeed: 5
+      frameSpeed: 5,
     },
     RunLeft: {
-      imageSrc:'Assets/FoxRunLeftSheet.png',
+      imageSrc: "Assets/FoxRunLeftSheet.png",
       frameRate: 8,
-      frameSpeed: 5
+      frameSpeed: 5,
     },
     Jump: {
-      imageSrc:'Assets/FoxJumpStartSheet.png',
+      imageSrc: "Assets/FoxJumpStartSheet.png",
       frameRate: 2,
-      frameSpeed: 20
+      frameSpeed: 20,
     },
     JumpLeft: {
-      imageSrc:'Assets/FoxJumpLeftStartSheet.png',
+      imageSrc: "Assets/FoxJumpLeftStartSheet.png",
       frameRate: 2,
-      frameSpeed: 20
+      frameSpeed: 20,
     },
     Land: {
-      imageSrc:'Assets/FoxJumpEndSheet.png',
+      imageSrc: "Assets/FoxJumpEndSheet.png",
       frameRate: 3,
-      frameSpeed: 15
+      frameSpeed: 15,
     },
     LandLeft: {
-      imageSrc:'Assets/FoxJumpLeftEndSheet.png',
+      imageSrc: "Assets/FoxJumpLeftEndSheet.png",
       frameRate: 3,
-      frameSpeed: 15
+      frameSpeed: 15,
     },
-  }
+  },
 });
 
 const keys = {
@@ -141,8 +139,7 @@ const keys = {
   },
 };
 
-
- let score = 1
+let score = 1;
 
 const background = new Sprite({
   position: {
@@ -152,80 +149,74 @@ const background = new Sprite({
   imageSrc: "Assets/Background.png",
 });
 
-const backgroundImageHeight = 1024
+const backgroundImageHeight = 1024;
 
 const camera = {
   position: {
     x: 0,
     y: -backgroundImageHeight + scaledCanvas.height,
   },
-}
+};
 
-let animationID
+let animationID;
 function animate() {
- animationId = window.requestAnimationFrame(animate);
-//   c.fillStyle = "white";
-//   c.fillRect(0, 0, canvas.width, canvas.height);
+  animationId = window.requestAnimationFrame(animate);
+  //   c.fillStyle = "white";
+  //   c.fillRect(0, 0, canvas.width, canvas.height);
 
   c.save();
-  c.scale(2, 2)
-  c.translate(camera.position.x, camera.position.y )
+  c.scale(2, 2);
+  c.translate(camera.position.x, camera.position.y);
   background.update();
-  collisionBlocks.forEach((collisionBlock) =>{
-    collisionBlock.update()
-  })
+  collisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.update();
+  });
 
-  starBlocks.forEach((starBlock) =>{
-    starBlock.update()
-  })
+  starBlocks.forEach((starBlock) => {
+    starBlock.update();
+  });
 
-  player.horizontalCanvasCollision()
+  player.horizontalCanvasCollision();
   player.update();
 
   player.velocity.x = 0;
   if (keys.d.pressed) {
-    player.spriteSwitch('RunRight')
-    player.velocity.x = 3
-    player.lastDirection = 'right'
-    player.cameraPanLeft({canvas, camera})
+    player.spriteSwitch("RunRight");
+    player.velocity.x = 3;
+    player.lastDirection = "right";
+    player.cameraPanLeft({ canvas, camera });
   } else if (keys.a.pressed) {
-    player.spriteSwitch('RunLeft')
-    player.velocity.x = -3
-    player.lastDirection = 'left'
-    player.cameraPanRight({canvas, camera})
+    player.spriteSwitch("RunLeft");
+    player.velocity.x = -3;
+    player.lastDirection = "left";
+    player.cameraPanRight({ canvas, camera });
   } else if (player.velocity.y === 0) {
-    if (player.lastDirection === 'right') player.spriteSwitch('Idle')
-    else player.spriteSwitch('IdleLeft')
-    
+    if (player.lastDirection === "right") player.spriteSwitch("Idle");
+    else player.spriteSwitch("IdleLeft");
   }
 
   if (player.velocity.y < 0) {
-    player.cameraPanDown({canvas, camera})
-    if (player.lastDirection === 'right') player.spriteSwitch('Jump')
-    else player.spriteSwitch('JumpLeft')
+    player.cameraPanDown({ canvas, camera });
+    if (player.lastDirection === "right") player.spriteSwitch("Jump");
+    else player.spriteSwitch("JumpLeft");
   } else if (player.velocity.y > 0) {
-    player.cameraPanUp ({canvas, camera})
-    if (player.lastDirection === 'right') player.spriteSwitch('Land')
-    else player.spriteSwitch('LandLeft')
+    player.cameraPanUp({ canvas, camera });
+    if (player.lastDirection === "right") player.spriteSwitch("Land");
+    else player.spriteSwitch("LandLeft");
   }
-   
 
   c.restore();
 
- if (score === 9) {
-  winText.style.display = "flex";
-  starText.style.display = "none";
-} else {
-  winText.style.display = "none";
-  starText.style.display = "flex";
-}
-
-  
+  if (score === 9) {
+    winText.style.display = "flex";
+    starText.style.display = "none";
+  } else {
+    winText.style.display = "none";
+    starText.style.display = "flex";
+  }
 }
 
 animate();
-
-
 
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
@@ -251,4 +242,3 @@ window.addEventListener("keyup", (event) => {
       break;
   }
 });
-
